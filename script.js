532f36098f7f26d595e5494eabe7830ecfb09bee
HeloiseAndelhofs <heloise.andelhofs@proximus.be>
2024-02-09 15:00:48 +0100

    keyframes js,

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const startGame = document.querySelector(".start");
 const secondsCountdownc = document.getElementById("seconds");
+const score = document.querySelector(".score");
 const bushes = document.querySelectorAll(".bush");
 const yuumis = document.querySelectorAll(".yuumi");
-const keyframeAnimation = new KeyframeEffect
 
 // console.log(startGame, secondsCountdownc, bushes, yuumis);
 
-let time;
+let timeUp = false;
+
 
 //Calcul du temps random pour l'apparition de yuumi
 function randomYuumiTime(min, max) {
@@ -22,10 +23,57 @@ function randomBush(bushes) {
 
 // console.log(randomBush(bushes));
 
+
 //fonction pour faire apparaitre yuumi dans un buisson aléatoir pour une durée aléatoire
+
 function yuumiApparition() {
-   time = randomYuumiTime(500, 1200);
+    time = randomYuumiTime(2000, 5000);
    bush = randomBush(bushes);
+   const yuumiOut = bush.querySelector(".yuumi")
+   
+   //animation yuumi
+   const keyframeAnimation = new KeyframeEffect(
+       yuumis,
+       [
+           {offset: '0%',
+           opacity: 0.5,
+           transform: 'translateY: (-40px)'
+        },
+        {offset: '5%',
+        opacity: 1,
+        transform: 'translateY(-50px)'
+    },
+    {offset: '25%',
+    opacity: 1,
+            transform: 'translateY(-50px)'
+        },
+        {offset: '50%',
+        opacity: 1,
+        transform: 'translateY(-50px)'
+    },
+    {offset: '75%',
+    opacity: 1,
+    transform: 'translateY(-50px)'
+},
+{offset: '100%',
+ransform: 'translateY(100px)'
+}
+],
+{duration: time}
+)
+const animation = new Animation(keyframeAnimation)
+animation.play()
 }
 
+
+//fonction pour lancer le jeu
+
+function startGame() {
+        
+
+}
+
+
 //  console.log(yuumiApparition());
+
+//appels de fonction + event
